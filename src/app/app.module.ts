19c93886941c30602ea76fb5f416f1874c39d45a
Gonzalo Muñoz <gmqdev@gmail.com>
2020-10-09 12:57:21 +0200

    Fix keyup for iOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { todoReducer } from './todos/todo.reducer';
-
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
